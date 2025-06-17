a94aefd939c0bde74cab8bc0ebb00b4b637d3aeb
Inshiya Ravat <inshiya.ravat@simformsolutions.com>
2025-06-17 13:53:07 +0530

    handled values using state

--- src/component/OTP/Otp.tsx
+++ src/component/OTP/Otp.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./Otp.module.css";
 
 interface Props {
@@ -7,48 +7,62 @@ interface Props {
 }
 const Otp = ({ length, prefilled }: Props) => {
   const inputRefs = useRef<HTMLInputElement[]>([]);
+  const [values, setValues] = useState("");
   const arr = Array.from({ length });
   useEffect(() => {
     if (prefilled) {
-      const otpArray = prefilled.split("");
-      for (let i = 0; i < otpArray.length; i++) {
-        inputRefs.current[i].value = otpArray[i];
-      }
+      setValues(prefilled);
+      inputRefs.current[inputRefs.current.length-1].focus();
     }
   }, [prefilled]);
   function handleChange(index: number) {
     if (index + 1 >= length) {
+      setValues((prevVal) => {
+        return prevVal.concat(inputRefs.current[index].value);
+      });
+      console.log("out front")
       return;
     } else {
+      setValues((prevVal) => {
+        return prevVal.concat(inputRefs.current[index].value);
+      });
       inputRefs.current[index + 1].focus();
+      inputRefs.current[index + 1].select();
     }
   }
+  
   function handleKeyDown(
     e: React.KeyboardEvent<HTMLInputElement>,
     index: number
   ) {
     if (e.key === "Backspace") {
-      if (index - 1 < 0) {
+      if (index-1 < 0) {
+        setValues((prevVal) => {
+          return prevVal.substring(0, prevVal.length - 1);
+        });
+        console.log("out back")
         return;
       } else {
         e.preventDefault();
-        inputRefs.current[index].value = "";
+        setValues((prevVal) => {
+          return prevVal.substring(0, prevVal.length - 1);
+        });
         inputRefs.current[index - 1].focus();
+        inputRefs.current[index - 1].select();
       }
     }
   }
   function handlePaste(e: React.ClipboardEvent<HTMLInputElement>) {
     e.preventDefault();
     const otpString = (e.clipboardData || window.Clipboard).getData("text");
-    const otpArray = otpString.split("");
-    for (let i = 0; i < otpArray.length; i++) {
-      inputRefs.current[i].value = otpArray[i];
-    }
+    setValues(otpString);
   }
+  const otpValue = values.split("");
   return (
     <>
       {arr.map((inputEl, index) => (
         <input
+          maxLength={1}
           className={styles.inputField}
           autoFocus={index === 0}
           ref={(refer) => {
@@ -57,11 +71,13 @@ const Otp = ({ length, prefilled }: Props) => {
             }
           }}
           key={index}
+          value={index < otpValue.length ? otpValue[index]: ""}
           onPaste={handlePaste}
           onChange={() => handleChange(index)}
           onKeyDown={(e) => handleKeyDown(e, index)}
         />
       ))}
+      <p>{values}</p>
     </>
   );
 };
